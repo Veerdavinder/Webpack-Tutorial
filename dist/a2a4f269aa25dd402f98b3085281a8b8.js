739dbea0919710c1159b3d3db7890773484d713e
Veer_Davinder <birdevinder1705@gmail.com>
2019-11-23 18:18:49 -0500

    learning webpack plugin

--- dist/a2a4f269aa25dd402f98b3085281a8b8.js
+++
@@ -1,4 +0,0 @@
-import helloWorld from './hello-world';
-import addImage from './add-image.js';
-helloWorld();
-addImage();
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require('path');
-
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry:'./src/index.js',
     output:{
-        filename:'bundle.js',
+        filename:'bundle.[contenthash].js',
         path:path.resolve(__dirname,'./dist'),
-        publicPath:'dist/'
+        publicPath:''
     },
     mode:'none',
     module:{
@@ -19,13 +22,13 @@ module.exports = {
             {
                 test:/\.css$/,
                 use:[
-                    'style-loader','css-loader'
+                   MiniCssExtractPlugin.loader,'css-loader'
                 ]
             },
             {
                 test:/\.scss$/,
                 use:[
-                    'style-loader','css-loader','sass-loader'
+                    MiniCssExtractPlugin.loader,'css-loader','sass-loader'
                 ]
             },
             {
@@ -38,7 +41,32 @@ module.exports = {
                         plugins:['transform-class-properties']
                     }
                 }
+            },
+            {
+                test:/\.hbs$/,
+                use:[
+                    'handlebars-loader'
+                ]
             }
         ]
-    }
+    },
+    plugins:[
+        new TerserPlugin(),
+        new MiniCssExtractPlugin({
+            filename:'styles.[contenthash].css'
+        }),
+        new CleanWebpackPlugin(
+            //{
+        //     cleanOnceBeforeBuildPatterns:[
+        //         '**/*',
+        //         path.join(process.cwd(),'build/**/*')
+        //     ]
+        // }
+        ),
+        new HtmlWebpackPlugin({
+            title:'Hello World',
+            template:'src/index.hbs',
+            description:'Some description'
+        })
+    ]
 }
